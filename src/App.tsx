a4f86b6c4fffc379076fd78f32bf059fa1d69698
Oskar Jiang <oskarjiang94@gmail.com>
2025-05-16 23:59:51 +0200

    fix: add logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import { ThemeProvider, CssBaseline } from '@mui/material';
+import { ThemeProvider, CssBaseline, Box } from '@mui/material';
 import Dashboard from './components/Dashboard';
+import Header from './components/Header';
 import theme from './theme';
 
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Dashboard />
+      <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+        <Header />
+        <Box component="main" sx={{ flexGrow: 1 }}>
+          <Dashboard />
+        </Box>
+      </Box>
     </ThemeProvider>
   );
 };
---
+++ src/components/Header.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { AppBar, Toolbar, Box, Container } from '@mui/material';
+
+const Header: React.FC = () => {
+    return (
+        <AppBar 
+            position="static" 
+            elevation={0}
+            sx={{ 
+                bgcolor: 'background.paper',
+                borderBottom: '1px solid',
+                borderColor: 'divider'
+            }}
+        >
+            <Container maxWidth="xl">
+                <Toolbar sx={{ px: { xs: 0 } }}>
+                    <Box
+                        component="img"
+                        src="/logo-black.388b1619.svg"
+                        alt="Logikfabriken"
+                        sx={{
+                            height: 40,
+                            width: 'auto',
+                            filter: 'brightness(0) saturate(100%) invert(0%) sepia(0%) saturate(0%) hue-rotate(93deg) brightness(95%) contrast(104%)'
+                        }}
+                    />
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
+};
+
+export default Header; 
