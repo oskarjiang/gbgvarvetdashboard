ef03732fede5896a6c6333a39f10d04368175ac4
Oskar Jiang <ojiang@volvocars.com>
2025-05-17 09:23:12 +0200

    fix: centralize logo
    Signed-off-by: Oskar Jiang <ojiang@volvocars.com>

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,33 +1,36 @@
-import React from 'react';
-import { AppBar, Toolbar, Box, Container } from '@mui/material';
+import React from "react";
+import { AppBar, Toolbar, Box, Container } from "@mui/material";
 
 const Header: React.FC = () => {
-    return (
-        <AppBar 
-            position="static" 
-            elevation={0}
-            sx={{ 
-                bgcolor: 'background.paper',
-                borderBottom: '1px solid',
-                borderColor: 'divider'
-            }}
+  return (
+    <AppBar
+      position="static"
+      elevation={0}
+      sx={{
+        bgcolor: "background.paper",
+        borderBottom: "1px solid",
+        borderColor: "divider",
+      }}
+    >
+      <Container maxWidth="xl">
+        <Toolbar
+          sx={{ px: { xs: 0 }, display: "flex", justifyContent: "center" }}
         >
-            <Container maxWidth="xl">
-                <Toolbar sx={{ px: { xs: 0 } }}>
-                    <Box
-                        component="img"
-                        src="/logo-black.388b1619.svg"
-                        alt="Logikfabriken"
-                        sx={{
-                            height: 40,
-                            width: 'auto',
-                            filter: 'brightness(0) saturate(100%) invert(0%) sepia(0%) saturate(0%) hue-rotate(93deg) brightness(95%) contrast(104%)'
-                        }}
-                    />
-                </Toolbar>
-            </Container>
-        </AppBar>
-    );
+          <Box
+            component="img"
+            src="/logo-black.388b1619.svg"
+            alt="Logikfabriken"
+            sx={{
+              height: 40,
+              width: "auto",
+              filter:
+                "brightness(0) saturate(100%) invert(0%) sepia(0%) saturate(0%) hue-rotate(93deg) brightness(95%) contrast(104%)",
+            }}
+          />
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 };
 
-export default Header; 
+export default Header;
